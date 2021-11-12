f32d3daf180d830c9b563820f1f87293a19f6c28
Rachel Bock <rachel.online@gmail.com>
2021-11-12 09:09:12 -0700

    Prayer Pattern defined

---
+++ shared/prayerPattern.js
@@ -0,0 +1,32 @@
+const PRAYERPATTERN = [
+    {
+        id: 0, 
+        step: "Prepare spiritually to speak with God",
+        length: 10000 ms
+    },
+    {
+        id: 1, 
+        step: "Address Heavenly Father",
+        length: 2000 ms
+    },
+    {
+        id: 2, 
+        step: "Express Gratitude for blessings and life experiences",
+        length: 60000 ms
+    },
+    {
+        id: 3, 
+        step: "Ask for what you stand in need of",
+        length: 60000 ms
+    },
+    {
+        id: 4, 
+        step: "Pray for others",
+        length: 60000 ms
+    },
+    {
+        id: 5, 
+        step: "End by saying 'in the name of Jesus Christ. Amen.'",
+        length: 2000 ms
+    }
+]
